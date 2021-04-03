175e6ddf8b0d4236d527d21709745575e76689b1
anggardhanoano <anggardhanoano@gmail.com>
2021-04-04 01:27:21 +0700

    finalize number 3

--- TK1/TK1_nomor3.m
+++ TK1/TK1_nomor3.m
@@ -6,7 +6,7 @@
 % Ux = y => Backward Substitution
 
 
-for k=1:3
+for k=1:11
   n = 10 * 2**k;
 
   A = bmatrix(n);
@@ -17,6 +17,6 @@ for k=1:3
   yaprox = BackwardSolution(U, y);
 
   yexact = exactsolution(n);
-  tb = 0.2*1:n;
-  yaprox(n)-yexact(n)
+  condition_number = cond(A)
+  error = yaprox(n)-yexact(n)
 end
